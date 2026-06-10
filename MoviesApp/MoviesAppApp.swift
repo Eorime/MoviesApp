6137c5c7f22b773c106146ebcc61071fc2e61368
Elene <elenedgebuadze83@gmail.com>
2026-06-10 11:12:11 +0400

    first commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Elene Dgebuadze on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
